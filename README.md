e0f8e0333733695bc20dc9cd61bafad74438b7ee
Vinicius Pegorini <viniciuspegorini@gmail.com>
2023-05-08 15:07:35 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Aulas da disciplina de Programação para Web - PW25S-5SI
+
+## Back-end 
+
+API REST desenvolvida utilizando o *framework* **Spring**.
+
+### Softwares
+	- JDK 11 ou superior
+	- IDE:
+		- ItelliJ IDEA
+		- Spring Tools Suite 4
+		- Eclipe for JavaEE ...
+	- SDBG:
+		- Postgresql
+	- Ferramenta para testar a API:
+		- Postman
+		- Insomnia
+	- Git
+	- Docker
+	
+## Front-end 
+
+Cliente web desenvolvido utilizando a biblioteca **React**.
+
+### Softwares
+	- IDE:
+		- Visual Studio Code
+		- Web Storm
+		- Atom...
+	- Git
+	- Node.js
+	- Docker
---
+++ aula1/server/src/main/java/br/edu/utfpr/pb/pw25s/server/model/Category.java
@@ -0,0 +1,31 @@
+package br.edu.utfpr.pb.pw25s.server.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(min = 2, max = 50)
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(name = "test_date")
+    private LocalDate testDate;
+}
